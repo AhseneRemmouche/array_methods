267ac58fcc28b36a2d4f6f8b18f71fb6ffcdbfa4
AhseneRemmouche <ahsene.remmouche@outlook.com>
2024-11-16 21:25:19 -0500

    forEach code refactoring

--- 1-forEach/script.js
+++ 1-forEach/script.js
@@ -1,16 +1,20 @@
-console.log('Bismi Allah')
+console.log("Bismi Allah");
 /**forEach method execute a call back function on each element in the array and return undefined*/
 /*-----Count Duplicate Letters--------*/
 
-const letters = ['a', 'b', 'c', 'd', 'a', 'b', 'e', 'd', 'a', 'e', 'f', 'd'];
-const count = {}
+const letters = ["a", "b", "c", "d", "a", "b", "e", "d", "a", "e", "f", "d"];
+const lettersCounter = {};
 
-const num = letters.forEach((value, index, arr) => {
-  if (!count[value]) {
-    count[value] = 1
-  } else {
-    count[value]++
-  }
-})
-console.log(count)
-console.log(num)
+letters.forEach((item, index, arr) => {
+	// if (!count[value]) {
+	//   count[value] = 1
+	// } else {
+	//   count[value]++
+	// }
+
+	// code refactoring
+	lettersCounter[item]
+		? (lettersCounter[item] += 1)
+		: (lettersCounter[item] = 1);
+});
+console.log(lettersCounter);
