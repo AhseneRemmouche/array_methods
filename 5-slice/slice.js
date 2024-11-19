668811f14586215af287b8c939cb40476fefa547
remmouche <121981104+remmouche@users.noreply.github.com>
2024-11-19 16:09:07 +0000

    update slice and splice methods

--- 5-slice/slice.js
+++ 5-slice/slice.js
@@ -1,28 +1,29 @@
 console.log('Bismi Allah')
 
-/** slice method return a shallow copy of a portion of an array it won't modify */
+/** The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included). The original array will not be modified. */
+
 /**slice takes two argument, the first is the starting index (by default it is 0) and the second is the ending index (by default it is the array length) and the ending index is not included*/
 
 /**------------example 1 ------------- */
 console.log('------------example 1 -------------');
 const numbers = [1, 2, 3, 4, 5]
-const numbers1 = numbers.slice(1,4)
+const numbers1 = numbers.slice(1,4) 
 
-console.log(numbers1)
-console.log(numbers)
+console.log(numbers1) //[2, 3, 4]
+console.log(numbers) //[1, 2, 3, 4, 5]
 
 /**------------example 2 slice without parameter provide a shallow copy of an array------------- */
 console.log('------------example 2 -------------');
 const numbers2 = [1, 2, 3, 4, 5]
 const numbers3 = numbers.slice()
 
-console.log(numbers2)
-console.log(numbers3)
+console.log(numbers2) //[1, 2, 3, 4, 5]
+console.log(numbers3) //[1, 2, 3, 4, 5]
 
 /**------------example 3 slice with negative value start from the end of the array------------- */
-console.log('------------example 2 -------------');
+console.log('------------example 3 -------------');
 const numbers4 = [1, 2, 3, 4, 5]
 const numbers5 = numbers.slice(-3)
 
-console.log(numbers4)
-console.log(numbers5)
+console.log(numbers4) //[1, 2, 3, 4, 5]
+console.log(numbers5) //[3, 4, 5]
--- 6-splice/splice.js
+++ 6-splice/splice.js
@@ -2,6 +2,8 @@ console.log("Bismi Allah");
 
 /**--splice change the array by removing or replacing exesting elemrnt from the array it gets starting index, count (means how may elements you want to remove) and it can gets optionnaly new elements who will be placed inside the array then it will change the array and return an array of the deleted elements */
 
+/* The splice() method changes the contents of an array by removing, replacing, or adding elements. It modifies the original array and returns an array containing the deleted elements, if any. */
+
 /**----------------example 1 remove 3 numbers from index 2 and replace them with 10 and 11 -------------------- */
 console.log("------------------ example 1 -----------------");
 const numbers = [1, 2, 3, 4, 5, 6];
@@ -12,5 +14,6 @@ console.log(deleted);
 /**--- example 2 if we put 0 as second parameter we can add number to the array but it will change the array and it is not the best way to do it-----*/
 console.log("------------example 2--------------- ");
 const numbers1 = [1, 2, 3, 4, 5, 6];
-numbers1.splice(2, 0, 11, 12, 13);
-console.log(numbers1);
+const deleted2 = numbers1.splice(2, 0, 11, 12, 13);
+console.log(numbers1); //[1, 2, 11, 12, 13, 3, 4,  5,  6]
+console.log(deleted2); //[]
